9dd93b2e7bf843d69e184cef7b9312ad52de5bfd
pchchv <ipchchv@gmail.com>
2025-03-15 12:55:25 +0300

    Implement a Reset and a Next methods

--- message.go
+++ message.go
@@ -32,6 +32,36 @@ func New(data []byte) *Message {
 	}
 }
 
+// Reset will set the index to 0 so the message can be read again.
+// Optionally pass in new data to reuse the Message object.
+func (m *Message) Reset(newData []byte) {
+	if newData != nil {
+		m.Data = newData
+	}
+
+	m.err = nil
+	m.Index = 0
+	m.fieldNumber = 0
+	m.wireType = 0
+}
+
+// Next will move the scanner to the next value.
+// Should be used in a for loop.
+func (m *Message) Next() bool {
+	if m.err == nil && m.Index < len(m.Data) {
+		if val, err := m.Varint64(); err != nil {
+			m.err = err
+			return false
+		} else {
+			m.fieldNumber = int(val >> 3)
+			m.wireType = int(val & 0x7)
+			return true
+		}
+	}
+
+	return false
+}
+
 func (m *Message) packedLength() (l int, err error) {
 	var l64 uint64
 	m.Index, l64, err = varint64(m.Data, m.Index)
