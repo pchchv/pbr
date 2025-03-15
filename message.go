b5cd54e1457116d25cfa3a28b00335f6b07d27f2
pchchv <ipchchv@gmail.com>
2025-03-15 11:58:26 +0300

    Add a Message type and a constructor

--- message.go
+++ message.go
@@ -8,3 +8,21 @@ type base struct {
 	Data  []byte
 	Index int
 }
+
+// Message is a container for a protobuf message type ready to be scanned.
+type Message struct {
+	base
+	err         error
+	fieldNumber int
+	wireType    int
+}
+
+// New creates a new Message scanner for the given encoded protobuf data.
+func New(data []byte) *Message {
+	return &Message{
+		base: base{
+			Data:  data,
+			Index: 0,
+		},
+	}
+}
