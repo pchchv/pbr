a83dee8b2c067c35225b390102670e16ed983c50
pchchv <ipchchv@gmail.com>
2025-03-16 06:40:30 +0300

    Implement a methods Count and Skip

--- iterator.go
+++ iterator.go
@@ -28,3 +28,51 @@ func (m *Message) Iterator(iter *Iterator) (*Iterator, error) {
 
 	return iter, nil
 }
+
+// Count returns the total number of values in the given repeating field.
+// The answer depends on the type/encoding or the field:
+// double, float, fixed, sfixed are WireType32bit or WireType64bit,
+// all other types (int, uint, sint) are WireTypeVarint.
+// Any other value will cause the function to panic.
+func (i *Iterator) Count(wireType int) (count int) {
+	switch wireType {
+	case WireType32bit:
+		return len(i.base.Data) / 4
+	case WireType64bit:
+		return len(i.base.Data) / 8
+	case WireTypeVarint:
+		for _, b := range i.Data {
+			if b < 128 {
+				count++
+			}
+		}
+		return
+	default:
+		panic("invalid wire type for a packed repeated field")
+	}
+}
+
+// Skip will move the interator forward 'count' value without actually reading it.
+// For a new iterator,
+// 'count' will move the pointer so that the next value call will be the 'counth' value.
+// The correct wireType must be specified:
+// double, float, fixed, sfixed are WireType32bit or WireType64bit,
+// all other types (int, uint, sint) are WireTypeVarint.
+// Any other value will cause the function to panic.
+func (i *Iterator) Skip(wireType int, count int) {
+	switch wireType {
+	case WireTypeVarint:
+		for j := 0; j < count; j++ {
+			for i.Data[i.Index] >= 128 {
+				i.Index++
+			}
+			i.Index++
+		}
+	case WireType32bit:
+		i.Index += 4 * count
+	case WireType64bit:
+		i.Index += 8 * count
+	default:
+		panic("invalid wire type for a packed repeated field")
+	}
+}
