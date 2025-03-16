8725067ecfd5d3d748a55399dfaa8e74ea83e589
pchchv <ipchchv@gmail.com>
2025-03-16 06:28:48 +0300

    Implement an Iterator method

--- iterator.go
+++ iterator.go
@@ -6,3 +6,25 @@ type Iterator struct {
 	base
 	fieldNumber int
 }
+
+// Iterator will use the current field.
+// Field must be a packed repeated field.
+func (m *Message) Iterator(iter *Iterator) (*Iterator, error) {
+	l, err := m.packedLength()
+	if err != nil {
+		return nil, err
+	}
+
+	if iter == nil {
+		iter = &Iterator{}
+	}
+
+	iter.base = base{
+		Data:  m.Data[m.Index : m.Index+l],
+		Index: 0,
+	}
+	iter.fieldNumber = m.fieldNumber
+	m.Index += l
+
+	return iter, nil
+}
